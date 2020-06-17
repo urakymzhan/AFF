bfd3a40a8560e11bef6bcfcb5c0856faf9589e02
Ulan Rakymzhanov <ulan@carta.healthcare>
2020-06-17 01:31:12 -0700

    sc

--- client/src/App.js
+++ client/src/App.js
@@ -5,20 +5,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h3>Asian Film Festivals</h3>
+        <div>
+          Create react project - Guliza<br/>
+          Come up with react libraries to use - bootstrap, googe map, etc? - Kushtar<br/>
+          Come up with components - Aselia, Aidai<br/>
+          state data, state data for every component  - Aselia, Aidai<br/>
+          How to insert google map location? Coordinates? longitude/latitude? Is it free? - Adilet<br/>
+          Pagination - buttons? Scroll? - Uzak<br/>
+          Filtering data - how to filter - Adilet<br/>
+        </div>
     </div>
   );
 }
